b7676c1727692a06f718bbb38ac237001fac3a1b
Ilkka Otsala <ilkka.otsala@gmail.com>
2025-11-28 10:45:10 +0200

    Image upload API

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -90,4 +90,13 @@ class RecipeSerializer(serializers.ModelSerializer):
 class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for recipe detail view."""
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description', 'image']
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images for recipe."""
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': True}}
